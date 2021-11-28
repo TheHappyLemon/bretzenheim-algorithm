643a4c77da6a7df9242dd1f543eb0c3225b89abb
Artem Kucheravy <artem.kucheravy@gmail.com>
2021-11-28 22:16:13 +0200

    add Brezenheim drawing method and delete resizeable canvas

--- main.py
+++ main.py
@@ -5,53 +5,72 @@ class App:
     def __init__(self):
         super().__init__()
         self.root = tk.Tk()
-        self.CANVAS_WIDTH = 800
+        self.root.resizable(False,False)
+        self.CANVAS_WIDTH = 850
         self.CANVAS_HEIGHT = 600
-        self.canvas = tk.Canvas(self.root, bg='white', width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT)
-        tmp = tk.Label(master=self.canvas,text='0',width=4,bg='white') # WIDTH IS IN CHARACTERS!!!!!!!!!!!!!!!
-        tmp.place(x=0,y=0)
-        self.root.update()
-        self.LABEL_WIDTH = tmp.winfo_width()
-        self.LABEL_HEIGHT = tmp.winfo_reqheight()
+        self.COORD_WIDTH = self.CANVAS_WIDTH
+        self.COORD_HEIGHT = self.CANVAS_HEIGHT - 200
         self.LABEL_DISTANCE = 100
-        self.root.update()
-        print(self.canvas.winfo_reqwidth())
-        self.canvas.pack(fill='both', expand=True)  # make canvas resizeable automatically
-        self.root.bind('<Configure>', self.on_resize)
+        self.canvas = tk.Canvas(self.root, bg='white', width=self.CANVAS_WIDTH, height=self.CANVAS_HEIGHT)
         self.canvas.pack()
+        self.my_grid = tk.PhotoImage(width=self.CANVAS_WIDTH,height=self.CANVAS_HEIGHT)
+        self.canvas.create_image((self.CANVAS_WIDTH/2, self.CANVAS_HEIGHT/2), image=self.my_grid, state="normal")
         self.root.title('Object trajetory')
         self.labels = [[],[]]
         self.draw_system()
 
+    def draw_line(self, x1, y1, x2, y2, color):
+        dx = abs(x2 - x1)
+        dy = abs(y2 - y1)
+        if x2 > x1:
+            xs = 1
+        else:
+            xs = -1
+        if y2 > y1:
+            ys = 1
+        else:
+            ys = -1
+        x = x1
+        y = y1
+        if dx > dy:
+            p = 2 * dy - dx
+            while x != x2:
+                x = x + xs
+                if p > 0:
+                    p = p + 2 * dy - 2 * dx
+                    y = y + ys
+                else:
+                    p = p + 2 * dy
+                self.my_grid.put(color,(x,y))
+        else:
+            p = 2 * dx - dy
+            while y != y2:
+                y = y + ys
+                if p > 0:
+                    p = p + 2 * dx - 2 * dy
+                    x = x + xs
+                else:
+                    p = p + 2 * dx
+                self.my_grid.put(color,(x,y))
 
     def add_coord_label(self, x, y, text):
-        print('creating label at',x,y)
-        label = tk.Label(self.canvas, text=text, bg='white')
+        #print('creating label at',x,y)
+        label = tk.Label(self.canvas, text=text, bg='white',anchor='w')
         label.place(x=x,y=y)
         if y == 0:
             self.labels[0].append(label)
         else:
             self.labels[1].append(label)
 
-
-    def on_resize(self, event):
-        deltax = self.canvas.winfo_width() - self.CANVAS_WIDTH
-        deltay = self.canvas.winfo_height() - self.CANVAS_HEIGHT
-        if deltax >=self.LABEL_DISTANCE + self.LABEL_WIDTH:
-            self.add_coord_label(self.labels[0][-1].winfo_x() + self.LABEL_DISTANCE,0,
-                                int(self.labels[0][-1].cget('text')) + 100)
-            self.CANVAS_WIDTH = self.canvas.winfo_width()
-        if deltay >= self.LABEL_DISTANCE + self.LABEL_WIDTH:
-            self.add_coord_label(0, self.labels[1][-1].winfo_y() + self.LABEL_DISTANCE,
-                            int(self.labels[1][-1].cget('text')) + 100)
-            self.CANVAS_HEIGHT = self.canvas.winfo_height()
-
-
     def draw_system(self):
-        for i in range(1,self.CANVAS_WIDTH // self.LABEL_DISTANCE + 1):
-            self.add_coord_label(x=i * 100,y=0,text=i * 100)
-        for i in range(1, (self.CANVAS_HEIGHT - 200) // self.LABEL_DISTANCE + 1):
-            self.add_coord_label(x=0,y=i * 100,text=i * 100)
+        self.add_coord_label(0,0,'0')
+        for i in range(1,self.COORD_WIDTH // self.LABEL_DISTANCE + 1):
+            self.add_coord_label(x=i * 100 - 10,y=0,text=i * 100)
+            print(i * 100,0,i * 100,self.COORD_HEIGHT)
+            self.draw_line(i * 100,0,i * 100,self.COORD_HEIGHT,'#a0a3a1')
+        for i in range(1,(self.COORD_HEIGHT) // self.LABEL_DISTANCE + 1):
+            self.add_coord_label(x=0,y=i * 100 - 10,text=i * 100)
+            self.draw_line(0,i * 100,self.COORD_WIDTH,i * 100,'#a0a3a1')
         self.root.update()
 
 
