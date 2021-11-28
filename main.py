e5fd8262be7e13f0d0d2d4404ba33d9eb29f0fdb
Artem Kucheravy <artem.kucheravy@gmail.com>
2021-11-28 21:00:40 +0200

    make system resize, but it works really strange, so I abandon this idea

--- main.py
+++ main.py
@@ -13,36 +13,47 @@ class App:
         self.root.update()
         self.LABEL_WIDTH = tmp.winfo_width()
         self.LABEL_HEIGHT = tmp.winfo_reqheight()
-        self.canvas.config(width=self.CANVAS_WIDTH+self.LABEL_WIDTH)
+        self.LABEL_DISTANCE = 100
+        self.root.update()
+        print(self.canvas.winfo_reqwidth())
         self.canvas.pack(fill='both', expand=True)  # make canvas resizeable automatically
         self.root.bind('<Configure>', self.on_resize)
         self.canvas.pack()
         self.root.title('Object trajetory')
+        self.labels = [[],[]]
         self.draw_system()
-        self.labels= [[],[]]
+
+
+    def add_coord_label(self, x, y, text):
+        print('creating label at',x,y)
+        label = tk.Label(self.canvas, text=text, bg='white')
+        label.place(x=x,y=y)
+        if y == 0:
+            self.labels[0].append(label)
+        else:
+            self.labels[1].append(label)
 
 
     def on_resize(self, event):
         deltax = self.canvas.winfo_width() - self.CANVAS_WIDTH
         deltay = self.canvas.winfo_height() - self.CANVAS_HEIGHT
-        if deltax != 0:
-            pass
-            #pif deltax -
-        if deltay != 0:
-            pass
+        if deltax >=self.LABEL_DISTANCE + self.LABEL_WIDTH:
+            self.add_coord_label(self.labels[0][-1].winfo_x() + self.LABEL_DISTANCE,0,
+                                int(self.labels[0][-1].cget('text')) + 100)
+            self.CANVAS_WIDTH = self.canvas.winfo_width()
+        if deltay >= self.LABEL_DISTANCE + self.LABEL_WIDTH:
+            self.add_coord_label(0, self.labels[1][-1].winfo_y() + self.LABEL_DISTANCE,
+                            int(self.labels[1][-1].cget('text')) + 100)
+            self.CANVAS_HEIGHT = self.canvas.winfo_height()
 
 
     def draw_system(self):
-        for i in range(1,self.CANVAS_WIDTH // 100 + 1):
-            tmp = tk.Label(self.canvas, text=i * 100)
-            tmp.place(x=i * 100, y=0)
-            tmp.config(bg='white')
-            self.labels[0].append(tmp)
-        for i in range(1, self.CANVAS_HEIGHT // 100 + 1):
-            tmp = tk.Label(self.canvas, text=i * 100)
-            tmp.place(x=0, y=i * 100)
-            tmp.config(bg='white')
-            self.labels[1].append(tmp)
+        for i in range(1,self.CANVAS_WIDTH // self.LABEL_DISTANCE + 1):
+            self.add_coord_label(x=i * 100,y=0,text=i * 100)
+        for i in range(1, (self.CANVAS_HEIGHT - 200) // self.LABEL_DISTANCE + 1):
+            self.add_coord_label(x=0,y=i * 100,text=i * 100)
+        self.root.update()
+
 
 
 if __name__ == '__main__':
